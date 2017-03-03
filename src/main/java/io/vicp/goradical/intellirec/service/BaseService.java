3af9ae8a23e6782b55c607e647a7eb445e2ecf69
Kenny <kenny__jing@163.com>
2017-03-03 21:06:10 +0800

    BaseService接口定义及BaseServiceImpl实现

--- src/main/java/io/vicp/goradical/intellirec/service/BaseService.java
+++ src/main/java/io/vicp/goradical/intellirec/service/BaseService.java
@@ -1,29 +1,255 @@
 package io.vicp.goradical.intellirec.service;
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
+/**
+ * Created by Kenny on 2017/3/3.
+ *
+ * 提供对 BaseDao 的基本封装及一些自定义的基础业务方法
+ * @param <T>
+ */
 public interface BaseService<T> {
-	//写操作
-	void saveEntity(T t);
+
+	/**
+	 * 根据实体 id 检索实体对象
+	 *
+	 * @param id 序列化 id
+	 * @return 实体对象
+	 */
+	T loadEntity(Serializable id);
+
+	/**
+	 * 保存实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 * @return 对象保存成功后的对象序列化 id
+	 */
+	Serializable saveEntity(T t);
+
+	/**
+	 * 保存/更新实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
 	void saveOrUpdateEntity(T t);
+
+	/**
+	 * 更新实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
 	void updateEntity(T t);
+
+	/**
+	 * 实体对象的 merge 操作,详细说明参阅官方文档
+	 *
+	 * @param t 泛型化的实体对象 t
+	 * @return 实体对象
+	 */
+	T mergeEntity(T t);
+
+	/**
+	 * 持久化实体对象 t
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
+	void persistEntity(T t);
+
+	/**
+	 * 删除实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
 	void deleteEntity(T t);
-	void batchEntityByHQL(String hql, Object... objects);
-	//读操作
-	T loadEntity(Integer id);
-	T getEntity(Integer id);
-	List<T> findEntityByHQL(String hql, Object... objects);
+
+	/**
+	 * 刷新实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
+	void refreshEntity(T t);
+
+	/**
+	 * 获取实体对象
+	 *
+	 * @param id
+	 * @return
+	 */
+	T getEntity(Serializable id);
+
+	// 下边的方法是自定方法接口，主要是跟 Query 有关的操作
+
+	/**
+	 * 通过 Query 批量操作实体对象
+	 *
+	 * @param hql hql 语句
+	 */
+	int batchEntityByHQL(String hql);
+
+	/**
+	 * 通过 Query 批量操作实体对象
+	 *
+	 * @param hql     hql 语句
+	 * @param objects Object对象数组,通过参数位置方式给 hql 语句添加参数,不推荐使用
+	 */
+	int batchEntityByHQL(String hql, Object... objects);
+
+	/**
+	 * 批量操作实体对象
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 */
+	int batchEntityByHQL(String hql, Map<String, Object> params);
 
 	//执行原生SQL语句
-	void executeSQL(String sql, Object... objects);
+
+	/**
+	 * 通过 Query 执行原生 sql 语句
+	 *
+	 * @param sql 原生 sql 语句
+	 * @return 语句执行后受影响的数据行数
+	 */
+	int executeSQL(String sql);
+
+	/**
+	 * 通过 Query 执行原生 sql 语句
+	 *
+	 * @param sql     原生 sql 语句
+	 * @param objects Object对象数组,通过参数位置方式给 sql 语句添加参数
+	 * @return
+	 */
+	int executeSQL(String sql, Object... objects);
+
+	//读操作
+
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql hql 语句
+	 * @return 实体对象List
+	 */
+	List<T> findEntityByHQL(String hql);
+
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql  hql 语句
+	 * @param page 要检索数据的页数
+	 * @param rows 每页的数据行数
+	 * @return 实体对象 List
+	 */
+	List<T> findEntityByHQL(String hql, int page, int rows);
+
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql     hql 语句
+	 * @param objects Object对象数组, 通过参数位置方式给 hql 语句添加参数,不推荐使用
+	 * @return 实体对象 List
+	 */
+	List<T> findEntityByHQL(String hql, Object... objects);
+
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 * @return 实体对象 List
+	 */
+	List<T> findEntityByHQL(String hql, Map<String, Object> params);
+
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 * @param page   要检索数据的页数
+	 * @param rows   每页的数据行数
+	 * @return 实体对象 List
+	 */
+	List<T> findEntityByHQL(String hql, Map<String, Object> params, int page, int rows);
 
 	//单值检索，确保查询结果有且只有一条记录
+
+	/**
+	 * 通过 Query 执行单值检索,如聚合函数 sum(), count(), 要求 hql 语句执行后返回数据最多只有一条
+	 *
+	 * @param hql hql 语句
+	 * @return 单值检索得到 Object 对象
+	 */
+	Object uniqueResult(String hql);
+
+	/**
+	 * 通过 Query 执行单值检索,如聚合函数 sum(), count(), 要求 hql 语句执行后返回数据最多只有一条
+	 *
+	 * @param hql     hql 语句
+	 * @param objects Object 对象数据,通过参数位置方式给 hql 语句添加参数,不推荐使用
+	 * @return 单值检索得到的 Object 对象
+	 */
 	Object uniqueResult(String hql, Object... objects);
+
+	/**
+	 * 通过 Query 执行单值检索,如聚合函数 sum(), count(), 要求 hql 语句执行后返回数据最多只有一条
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 * @return 单值检索得到的 Object 对象
+	 */
+	Object uniqueResult(String hql, Map<String, Object> params);
+
+	//执行原生查询,可以指定是否封装成实体
+
+	/**
+	 * 执行原生 SQL 检索
+	 *
+	 * @param clazz 要检索的类对象,为空则表示不封装成实体
+	 * @param sql   sql 语句
+	 * @return List 对象
+	 */
+	List executeSQLQuery(Class clazz, String sql);
+
+	/**
+	 * 执行原生 SQL 检索
+	 *
+	 * @param clazz   要检索的类对象,为空则表示不封装成实体
+	 * @param sql     sql 语句
+	 * @param objects Object 对象数据,通过参数位置方式给 sql 语句添加参数
+	 * @return List 对象
+	 */
+	List executeSQLQuery(Class clazz, String sql, Object... objects);
+
+	/**
+	 * 得到检索结果的第一条数据
+	 *
+	 * @param hql hql 语句
+	 * @return 实体对象
+	 */
+	T getFirstEntity(String hql);
+
+	/**
+	 * 得到检索结果的第一条数据
+	 *
+	 * @param hql     hql 语句
+	 * @param objects Object 对象数组,通过参数位置方式给 hql 语句添加参数,不推荐使用
+	 * @return 实体对象
+	 */
+	T getFirstEntity(String hql, Object... objects);
+
+	/**
+	 * 得到检索结果的第一条数据
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 * @return 实体对象
+	 */
+	T getFirstEntity(String hql, Map<String, Object> params);
+
+
+
 	//查询所有实体
 	List<T> findAllEntities();
 
-	/**
-	 * 执行原生查询
-	 */
-	List executeSQLQuery(Class clazz, String sql, Object... objects);
 }
--- src/main/java/io/vicp/goradical/intellirec/service/impl/BaseServiceImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/service/impl/BaseServiceImpl.java
@@ -1,15 +1,17 @@
 package io.vicp.goradical.intellirec.service.impl;
 
 import io.vicp.goradical.intellirec.dao.BaseDao;
-import io.vicp.goradical.intellirec.dao.impl.BaseDaoImpl;
 import io.vicp.goradical.intellirec.service.BaseService;
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
+import java.util.Map;
 
 /**
+ * Created by Kenny on 2017/3/3.
+ *
  * 抽象的BaseService,专门用于继承
  * @param <T>
  */
@@ -27,62 +29,234 @@ public abstract class BaseServiceImpl<T> implements BaseService<T> {
 		clazz = (Class<T>) type.getActualTypeArguments()[0];
 	}
 
+	/**
+	 * 根据实体 id 检索实体对象
+	 *
+	 * @param id 序列化 id
+	 * @return 实体对象
+	 */
 	@Override
-	public void saveEntity(T t) {
-		dao.saveEntity(t);
+	public T loadEntity(Serializable id) {
+		return dao.loadEntity(id);
 	}
 
+	/**
+	 * 保存实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 * @return 对象保存成功后的对象序列化 id
+	 */
+	@Override
+	public Serializable saveEntity(T t) {
+		return dao.saveEntity(t);
+	}
+
+	/**
+	 * 保存/更新实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
 	@Override
 	public void saveOrUpdateEntity(T t) {
 		dao.saveOrUpdateEntity(t);
 	}
 
+	/**
+	 * 更新实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
 	@Override
 	public void updateEntity(T t) {
 		dao.updateEntity(t);
 	}
 
+	/**
+	 * 实体对象的 merge 操作,详细说明参阅官方文档
+	 *
+	 * @param t 泛型化的实体对象 t
+	 * @return 实体对象
+	 */
+	@Override
+	public T mergeEntity(T t) {
+		return dao.mergeEntity(t);
+	}
+
+	/**
+	 * 持久化实体对象 t
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
+	@Override
+	public void persistEntity(T t) {
+		dao.persistEntity(t);
+	}
+
+	/**
+	 * 删除实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
 	@Override
 	public void deleteEntity(T t) {
 		dao.deleteEntity(t);
 	}
 
+	/**
+	 * 刷新实体对象
+	 *
+	 * @param t 泛型化的实体对象 t
+	 */
 	@Override
-	public void batchEntityByHQL(String hql, Object... objects) {
-		dao.batchEntityByHQL(hql, objects);
+	public void refreshEntity(T t) {
+		dao.refreshEntity(t);
 	}
 
+	/**
+	 * 获取实体对象
+	 *
+	 * @param id
+	 * @return
+	 */
 	@Override
-	public void executeSQL(String sql, Object... objects) {
-		dao.executeSQL(sql, objects);
-	}
-
-	@Override
-	public T loadEntity(Integer id) {
-		return dao.loadEntity(id);
-	}
-
-	@Override
-	public T getEntity(Integer id) {
+	public T getEntity(Serializable id) {
 		return dao.getEntity(id);
 	}
 
+	/**
+	 * 通过 Query 批量操作实体对象
+	 *
+	 * @param hql hql 语句
+	 */
+	@Override
+	public int batchEntityByHQL(String hql) {
+		return dao.batchEntityByHQL(hql);
+	}
+
+	/**
+	 * 通过 Query 批量操作实体对象
+	 *
+	 * @param hql     hql 语句
+	 * @param objects Object对象数组,通过参数位置方式给 hql 语句添加参数,不推荐使用
+	 */
+	@Override
+	public int batchEntityByHQL(String hql, Object... objects) {
+		return dao.batchEntityByHQL(hql, objects);
+	}
+
+	/**
+	 * 批量操作实体对象
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 */
+	@Override
+	public int batchEntityByHQL(String hql, Map<String, Object> params) {
+		return dao.batchEntityByHQL(hql, params);
+	}
+
+	/**
+	 * 通过 Query 执行原生 sql 语句
+	 *
+	 * @param sql 原生 sql 语句
+	 * @return 语句执行后受影响的数据行数
+	 */
+	@Override
+	public int executeSQL(String sql) {
+		return dao.executeSQL(sql);
+	}
+
+	/**
+	 * 通过 Query 执行原生 sql 语句
+	 *
+	 * @param sql     原生 sql 语句
+	 * @param objects Object对象数组,通过参数位置方式给 sql 语句添加参数
+	 * @return
+	 */
+	@Override
+	public int executeSQL(String sql, Object... objects) {
+		return dao.executeSQL(sql, objects);
+	}
+
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql hql 语句
+	 * @return 实体对象List
+	 */
+	@Override
+	public List<T> findEntityByHQL(String hql) {
+		return dao.findEntityByHQL(hql);
+	}
+
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql  hql 语句
+	 * @param page 要检索数据的页数
+	 * @param rows 每页的数据行数
+	 * @return 实体对象 List
+	 */
+	@Override
+	public List<T> findEntityByHQL(String hql, int page, int rows) {
+		return dao.findEntityByHQL(hql, page, rows);
+	}
+
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql     hql 语句
+	 * @param objects Object对象数组, 通过参数位置方式给 hql 语句添加参数,不推荐使用
+	 * @return 实体对象 List
+	 */
 	@Override
 	public List<T> findEntityByHQL(String hql, Object... objects) {
 		return dao.findEntityByHQL(hql, objects);
 	}
 
+	/**
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 * @return 实体对象 List
+	 */
 	@Override
-	public List<T> findAllEntities() {
-		String hql = "from " + clazz.getSimpleName();
-		return findEntityByHQL(hql);
+	public List<T> findEntityByHQL(String hql, Map<String, Object> params) {
+		return dao.findEntityByHQL(hql, params);
 	}
 
 	/**
-	 * 单值检索,确保查询结果有且只有一条记录
-	 * @param hql
-	 * @param objects
-	 * @return
+	 * 通过 Query 检索实体对象
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 * @param page   要检索数据的页数
+	 * @param rows   每页的数据行数
+	 * @return 实体对象 List
+	 */
+	@Override
+	public List<T> findEntityByHQL(String hql, Map<String, Object> params, int page, int rows) {
+		return dao.findEntityByHQL(hql, params, page, rows);
+	}
+
+	/**
+	 * 通过 Query 执行单值检索,如聚合函数 sum(), count(), 要求 hql 语句执行后返回数据最多只有一条
+	 *
+	 * @param hql hql 语句
+	 * @return 单值检索得到 Object 对象
+	 */
+	@Override
+	public Object uniqueResult(String hql) {
+		return dao.uniqueResult(hql);
+	}
+
+	/**
+	 * 通过 Query 执行单值检索,如聚合函数 sum(), count(), 要求 hql 语句执行后返回数据最多只有一条
+	 *
+	 * @param hql     hql 语句
+	 * @param objects Object 对象数据,通过参数位置方式给 hql 语句添加参数,不推荐使用
+	 * @return 单值检索得到的 Object 对象
 	 */
 	@Override
 	public Object uniqueResult(String hql, Object... objects) {
@@ -90,21 +264,84 @@ public abstract class BaseServiceImpl<T> implements BaseService<T> {
 	}
 
 	/**
-	 * 仅作为SQL语句方便书写
-	 * @return
+	 * 通过 Query 执行单值检索,如聚合函数 sum(), count(), 要求 hql 语句执行后返回数据最多只有一条
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 * @return 单值检索得到的 Object 对象
 	 */
-	protected Session getCurrentSession() {
-		return ((BaseDaoImpl) dao).getSessionFactory().getCurrentSession();
+	@Override
+	public Object uniqueResult(String hql, Map<String, Object> params) {
+		return dao.uniqueResult(hql, params);
 	}
 
 	/**
-	 * 执行原生查询
+	 * 执行原生 SQL 检索
 	 *
-	 * @param sql
-	 * @param objects
+	 * @param clazz 要检索的类对象,为空则表示不封装成实体
+	 * @param sql   sql 语句
+	 * @return List 对象
+	 */
+	@Override
+	public List executeSQLQuery(Class clazz, String sql) {
+		return dao.executeSQLQuery(clazz, sql);
+	}
+
+	/**
+	 * 执行原生 SQL 检索
+	 *
+	 * @param clazz   要检索的类对象,为空则表示不封装成实体
+	 * @param sql     sql 语句
+	 * @param objects Object 对象数据,通过参数位置方式给 sql 语句添加参数
+	 * @return List 对象
 	 */
 	@Override
 	public List executeSQLQuery(Class clazz, String sql, Object... objects) {
 		return dao.executeSQLQuery(clazz, sql, objects);
+	}
+
+	/**
+	 * 得到检索结果的第一条数据
+	 *
+	 * @param hql hql 语句
+	 * @return 实体对象
+	 */
+	@Override
+	public T getFirstEntity(String hql) {
+		return dao.getFirstEntity(hql);
+	}
+
+	/**
+	 * 得到检索结果的第一条数据
+	 *
+	 * @param hql     hql 语句
+	 * @param objects Object 对象数组,通过参数位置方式给 hql 语句添加参数,不推荐使用
+	 * @return 实体对象
+	 */
+	@Override
+	public T getFirstEntity(String hql, Object... objects) {
+		return dao.getFirstEntity(hql, objects);
+	}
+
+	/**
+	 * 得到检索结果的第一条数据
+	 *
+	 * @param hql    hql 语句
+	 * @param params Map 对象,通过参数名方式给 hql 语句添加参数,推荐使用
+	 * @return 实体对象
+	 */
+	@Override
+	public T getFirstEntity(String hql, Map<String, Object> params) {
+		return dao.getFirstEntity(hql, params);
+	}
+
+	/**
+	 * 检索所有实体对象
+	 * @return 所有实体对象
+	 */
+	@Override
+	public List<T> findAllEntities() {
+		String hql = "from " + clazz.getSimpleName();
+		return findEntityByHQL(hql);
 	}
 }
