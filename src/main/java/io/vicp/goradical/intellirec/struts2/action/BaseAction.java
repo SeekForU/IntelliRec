59901d4bf24c68845f4038857f058624d853df73
Kenny <kenny__jing@163.com>
2017-03-04 20:12:48 +0800

    BaseAction实现,专门用于继承

--- src/main/java/io/vicp/goradical/intellirec/struts2/action/BaseAction.java
+++ src/main/java/io/vicp/goradical/intellirec/struts2/action/BaseAction.java
@@ -1,16 +1,29 @@
 package io.vicp.goradical.intellirec.struts2.action;
 
+import com.alibaba.fastjson.JSON;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import com.opensymphony.xwork2.Preparable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.ParentPackage;
 
 import java.lang.reflect.ParameterizedType;
 
 /**
+ * Created by Kenny on 2017/3/3.
+ *
  * 抽象Action，专门用于继承
  * @param <T>
  */
+@ParentPackage("basePackage")
 public class BaseAction<T> extends ActionSupport implements ModelDriven<T>, Preparable{
+	private static final Logger LOG = LogManager.getLogger(BaseAction.class);
+
+	/**
+	 * 泛型化模型 model
+	 */
 	protected T model;
 
 	public BaseAction() {
@@ -33,5 +46,17 @@ public class BaseAction<T> extends ActionSupport implements ModelDriven<T>, Prep
 	@Override
 	public void prepare() throws Exception {
 
+	}
+
+	protected void writeJson(Object object) {
+		try {
+			String jsonStr = JSON.toJSONStringWithDateFormat(object, "yyyy-MM-dd HH:mm:ss");
+			ServletActionContext.getResponse().setContentType("text/json;charset=utf-8");
+			ServletActionContext.getResponse().getWriter().write(jsonStr);
+			ServletActionContext.getResponse().getWriter().flush();
+			ServletActionContext.getResponse().getWriter().close();
+		} catch (Exception e) {
+			LOG.error(e.getMessage());
+		}
 	}
 }
