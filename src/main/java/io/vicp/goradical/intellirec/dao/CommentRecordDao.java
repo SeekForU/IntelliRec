5537de0f63db07f9ff40d39abdaa6a6d26f7552b
Kenny <kenny__jing@163.com>
2017-03-08 22:40:39 +0800

    CommentRecordDao添加统计视频播放量的方法，实现类进行实现

--- src/main/java/io/vicp/goradical/intellirec/dao/CommentRecordDao.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/CommentRecordDao.java
@@ -2,9 +2,17 @@ package io.vicp.goradical.intellirec.dao;
 
 import io.vicp.goradical.intellirec.model.pmrs.CommentRecord;
 
+import java.io.Serializable;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 public interface CommentRecordDao extends BaseDao<CommentRecord> {
+	/**
+	 * 根据视频id统计视频总的播放量
+	 * @param videoId 视频id
+	 * @return 统计总数
+	 */
+	long countTotalPlayWithVideoId(Serializable videoId);
 
 }
--- src/main/java/io/vicp/goradical/intellirec/dao/impl/CommentRecordDaoImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/impl/CommentRecordDaoImpl.java
@@ -4,10 +4,22 @@ import io.vicp.goradical.intellirec.dao.CommentRecordDao;
 import io.vicp.goradical.intellirec.model.pmrs.CommentRecord;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 @Repository("commentRecordDao")
 public class CommentRecordDaoImpl extends BaseDaoImpl<CommentRecord> implements CommentRecordDao {
 
+	@Override
+	public long countTotalPlayWithVideoId(Serializable videoId) {
+		String hql = "select  count(*) from CommentRecord cr where cr.video.id = :videoId";
+		Map<String, Object> params = new HashMap<>();
+		params.put("videoId", videoId);
+		long count = (long) uniqueResult(hql, params);
+		return count;
+	}
 }
