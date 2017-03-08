d85331a55964f703f062cfa62d0b32dd208f54d2
Kenny <kenny__jing@163.com>
2017-03-08 22:44:27 +0800

    CommentRecordDaoTest根据视频id统计视频播放量方法测试

---
+++ src/test/java/io/vicp/goradical/intellirec/dao/impl/CommentRecordDaoImplTest.java
@@ -0,0 +1,33 @@
+package io.vicp.goradical.intellirec.dao.impl;
+
+import io.vicp.goradical.intellirec.dao.CommentRecordDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Kenny on 2017/3/8.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class CommentRecordDaoImplTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+	private static final Logger LOG = LogManager.getLogger(CommentRecordDaoImplTest.class);
+
+	@Autowired
+	private CommentRecordDao crd;
+
+	@Test
+	public void countTotalPlay() throws Exception {
+		for (int i = 1; i < 3000 ; i++) {
+			long count = crd.countTotalPlayWithVideoId(i);
+			LOG.info(count);
+		}
+	}
+
+}
