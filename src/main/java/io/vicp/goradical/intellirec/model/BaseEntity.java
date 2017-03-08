41b32f7271d976346f4688186c9303ee4a3bcc65
Kenny <kenny__jing@163.com>
2017-03-08 16:48:27 +0800

    BaseEntity 类toString 方法修改,改用发射实现实体的基本属性值按照 json 字符串格式进行输出,避免懒加载异常

--- src/main/java/io/vicp/goradical/intellirec/model/BaseEntity.java
+++ src/main/java/io/vicp/goradical/intellirec/model/BaseEntity.java
@@ -1,8 +1,12 @@
 package io.vicp.goradical.intellirec.model;
 
-import com.alibaba.fastjson.JSON;
+import io.vicp.goradical.intellirec.util.StringUtil;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Date;
 
 /**
  * Created by Kenny on 2017/3/3.
@@ -32,10 +36,59 @@ public abstract class BaseEntity implements Serializable {
 	 *
 	 * @return
 	 */
+//	@Override
+//	public String toString() {
+//		String jsonStr = JSON.toJSONStringWithDateFormat(this, "yyyy-MM-dd HH:mm:ss");
+//		return jsonStr;
+//	}
+
+	/**
+	 * 利用反射将对象的基本属性按照类 json 格式进行输出
+	 * @return
+	 */
 	@Override
 	public String toString() {
-		String jsonStr = JSON.toJSONStringWithDateFormat(this, "yyyy-MM-dd HH:mm:ss");
-		return jsonStr;
+		try {
+			StringBuffer buffer = new StringBuffer();
+			Class clazz = getClass();
+			String simpleName = clazz.getSimpleName();
+			buffer.append(simpleName);
+			buffer.append(" {");
+			Field[] fields = clazz.getDeclaredFields();
+			Class ftype;
+			String fname;
+			Object fvalue;
+			for (Field field : fields) {
+				ftype = field.getType();
+				fname = field.getName();
+				field.setAccessible(true);
+//				fvalue = field.get(this);
+				if ((ftype.isPrimitive()
+						|| ftype == Integer.class
+						|| ftype == Long.class
+						|| ftype == Short.class
+						|| ftype == Boolean.class
+						|| ftype == Character.class
+						|| ftype == Float.class
+						|| ftype == Double.class
+						|| ftype == String.class
+						|| ftype == Date.class
+						|| ftype.isArray()) && !Modifier.isStatic(field.getModifiers())) {
+					//是否是基本数据类型
+					buffer.append(fname);
+					buffer.append(":");
+					Method method = clazz.getMethod("get" + StringUtil.toUpCaseFirstChar(fname));
+					fvalue = method.invoke(this);
+					buffer.append(fvalue);
+					buffer.append(", ");
+				}
+			}
+			buffer.delete(buffer.length() - 2, buffer.length());
+			buffer.append("}");
+			return buffer.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-
 }
